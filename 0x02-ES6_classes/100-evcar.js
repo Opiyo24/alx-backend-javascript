c5dbbda049e8a8933f77c131e9d74a0c590c9a88
Opiyo24 <gabbynyangor@gmail.com>
2024-03-07 15:17:49 +0300

    New car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this.range = range;
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-    get range() {
-        return this._range;
-    }
-
-    set range(rvalue) {
-        this._range = value;
-    }
-
-    cloneCar() {
-        const Species = super.constructor[Symbol.species];
-
-        return new Species();
-    }
+  static get [Symbol.species]() {
+    return Car;
+  }
 }
